a2d2393344a301f7a7049684a71277a2b05e49b1
Игорь Солоненко <F71251@gmail.com>
2023-05-30 14:11:57 +0300

    Add project files.

---
+++ PR9_ThunderstormBird/Mail.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf.Ui.Controls;
+using ImapX;
+using ImapX.Collections;
+using Org.BouncyCastle.Crypto;
+
+namespace PR9_ThunderstormBird
+{
+	internal class LoggedUser
+	{
+		public string Email { get; set; }
+		public string Pass { get; set; }
+		public string SmtpHost { get; set; }
+	}
+	internal class ImapHelper
+	{
+		private static ImapClient client { get; set; }
+		private static LoggedUser loggedUser { get; set; } = new LoggedUser();
+		public static void Initialize(string host)
+		{
+			client = new ImapClient(host, true);
+			if (!client.Connect())
+			{
+				throw new Exception("Error connecting to the client.");
+			}
+			else
+			{
+				loggedUser.SmtpHost = host.Replace("imap", "smtp");
+			}
+		}
+		public static bool Login(string u, string p)
+		{
+			loggedUser.Email = u;
+			loggedUser.Pass = p;
+			return client.Login(u, p);
+
+		}
+		public static void Logout()
+		{
+			// Remove the login value from the client.
+			if (client.IsAuthenticated)
+			{
+				client.Logout();
+				client.Dispose();
+			}
+		}
+		public static CommonFolderCollection GetFolders()
+		{
+			client.Folders.Inbox.StartIdling(); // And continue to listen for more.
+			client.Folders.Inbox.OnNewMessagesArrived += Inbox_OnNewMessagesArrived;
+			return client.Folders;
+		}
+		private static void Inbox_OnNewMessagesArrived(object sender, IdleEventArgs e)
+		{
+			// Show a dialog
+			System.Windows.MessageBox.Show($"Пришло новое сообщение в папку {e.Folder.Name}.");
+		}
+		public static MessageCollection GetMessagesForFolder(string name)
+		{
+			client.Folders[name].Messages.Download(); // Start the download process;
+			return client.Folders[name].Messages;
+		}
+		public static LoggedUser GetCredentials()
+		{
+			return loggedUser;
+		}
+	}
+}
---
+++ PR9_ThunderstormBird/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using Spire.Doc;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Interop;
+using System.Xml.Linq;
+using Wpf.Ui.Controls;
+using System.Windows.Forms;
+using Wpf.Ui.Appearance;
+using System.Threading.Tasks;
+
+namespace PR9_ThunderstormBird
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : UiWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+		private void MessagesLbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			try
+			{
+				File.WriteAllText("msg.html", message.messageslist[MessagesLbx.SelectedIndex].HtmlBody);
+				var d = new Document("msg.html", FileFormat.Html);
+				d.SaveToFile("msg.rtf", FileFormat.Rtf);
+				d.Close();
+				using (FileStream fileStream = new FileStream("msg.rtf", FileMode.Open))
+				{
+					Dispatcher.BeginInvoke(new Action(() =>
+					{
+						TextRange textRange = new TextRange(MailOpened.Document.ContentStart, MailOpened.Document.ContentEnd);
+						textRange.Load(fileStream, System.Windows.DataFormats.Rtf);
+					}));
+				}
+				File.Delete("msg.html");
+			}
+			catch (Exception er)
+			{
+				System.Windows.MessageBox.Show(er.Message);
+			}
+		}
+
+		private async void Update_Click(object sender, RoutedEventArgs e)
+		{
+			await LoadMailAsync();
+		}
+
+		public async Task LoadMailAsync()
+		{
+			using (var client = new ImapClient())
+			{
+				await client.ConnectAsync("imap.gmail.com", 993, true);
+				await client.AuthenticateAsync("f71251@gmail.com", "tlkpihvlhaioagci");
+
+				var inbox = client.Inbox;
+				await inbox.OpenAsync(FolderAccess.ReadOnly);
+
+				List<string> msg = new List<string>();
+				Progress.Maximum = 100;
+				Progress.Value = 0;
+
+				for (int i = 3570; i < 3580; i++)
+				{
+					
+					Progress.Value += 1;
+					try
+					{
+						var message = await inbox.GetMessageAsync(i);
+						string email = message.From.ToString();
+						int indexStart = email.IndexOf('"') + 1;
+						int indexEnd = email.IndexOf('"', indexStart);
+						string firstPart = email.Substring(indexStart, indexEnd - indexStart);
+						string secondPart = email.Substring(email.IndexOf('<') + 1, email.IndexOf('>') - email.IndexOf('<') - 1);
+						PR9_ThunderstormBird.message.messageslist.Add(new message(message.Subject.ToString(), secondPart, firstPart, message.Date.ToString(), message.HtmlBody.ToString()));
+						msg.Add(message.Subject + "\n" + firstPart + "\n" + secondPart + "\n" + message.Date);
+					}
+					catch { 
+					
+					}
+				}
+
+				MessagesLbx.ItemsSource= msg;
+			}
+		}
+
+	}
+	public class message
+	{
+		public static List<message> messageslist = new List<message>();
+
+		public string Subject;
+		public string From;
+		public string Name;
+		public string Date;
+		public string HtmlBody;
+
+		public message(string subject, string from, string name, string date, string htmlBody)
+		{
+			Subject = subject;
+			From = from;
+			Date = date;
+			HtmlBody = htmlBody;
+			Name = name;
+		}
+	}
+}
